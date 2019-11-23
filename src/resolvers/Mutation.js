174d5afa1839cd0f2b5bdd75a48b1d123cf4b00a
neword <newordn@gmail.com>
2019-11-23 17:17:01 +0100

    Added SignUp Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,15 @@
 const APP_SECRET = "POLYTECTCAMPUSAPPSECRET"
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 async function signUp(parent,args,context,info)
 {
-    
+    const password = await bcrypt.hash(args.password,10)
+    const user = await context.prisma.createUser({...args,password})
+    const token = jwt.sign({userId:user.id},APP_SECRET)
+    return {
+        token,user
+    }
 }
 async function logIn(parent,args,context,info)
 {
