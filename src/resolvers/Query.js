a55dc9bb49eeb2a1f7d087773cd3a0d0c0872807
neword <newordn@gmail.com>
2019-11-24 19:30:01 +0100

    Added crews query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -4,7 +4,12 @@ async function users(parent,args,context,info){
     const users = await context.prisma.users({})
     return users
 }
+async function crews(parent,args,context,info){
+    const crews = await context.prisma.crews({})
+    return crews
+}
 module.exports={
     info,
-    users
+    users,
+    crews
 }
