9b71fb5caeab3e51707c4650a0d7bab3fd194661
neword <newordn@gmail.com>
2019-12-06 08:47:47 +0100

    sorting queried elements DESC

--- src/resolvers/Crew.js
+++ src/resolvers/Crew.js
@@ -2,7 +2,7 @@ function users(parent,args,context,info){
 return context.prisma.crew({id:parent.id}).users()
 }
 function posts(parent,args,context,info){
-    return context.prisma.crew({id:parent.id}).posts()
+    return context.prisma.crew({id:parent.id}).posts({orderBy:'id_DESC'})
 }
 module.exports = {
     users,
--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,8 +1,8 @@
 function crews(parent,args,context,info){
-    return context.prisma.user({id:parent.id}).crews()
+    return context.prisma.user({id:parent.id}).crews({orderBy:'id_DESC'})
     }
     function posts(parent,args,context,info){
-        return context.prisma.user({id:parent.id}).posts()
+        return context.prisma.user({id:parent.id}).posts({orderBy:'id_DESC'})
     }
     module.exports = {
         crews,posts
