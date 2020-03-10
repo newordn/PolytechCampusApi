f21a90b4e29f51a39cad037a4eac3178a289b4ac
neword <newordn@gmail.com>
2020-03-10 11:11:20 +0100

    Updated upload helper and added credential.json

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -1,38 +1,36 @@
-const mkdirp = require('mkdirp')
-const UPLOAD_DIR='./uploads'
 const shortid = require('shortid')
 const { createWriteStream, unlink } = require('fs')
 const {google} = require('googleapis')
-const oauth2Client = new google.auth.OAuth2(
-  "350260510285-t7c9glq9o0gkcu3b3281qko95ruk2f5a.apps.googleusercontent.com",
-  "QNYfXH6tW__UD3w7Va0dB15L"
+const credentials = require('./credentials.json')
+const folder = '1x5XJjuegm8d9c60ip9TO7lXDCgyPYH6Z'
+const scopes = [
+  'https://www.googleapis.com/auth/drive'
+];
+const auth = new google.auth.JWT(
+  credentials.client_email,null,
+  credentials.private_key,scopes
 )
 
-oauth2Client.setCredentials({
-  refresh_token:"1//04zalNEh0DpyDCgYIARAAGAQSNwF-L9IriWFjE8GVYraE9bdLRkf_gmjrhV6blSDBYtTv6X-Hy9tj9Ouj3srGtX3ykomRGTYuxRE",
-  access_token:"ya29.Il-0BwvUj4DOeoSRGwGYTYMRwSyLqIOcyhdZb0LkTO9o0mbliMPpuGRcPvlrVZSvDyh8jpuX7ddvpnZK3gOey7M6gzN0iZkEZCv5sPJWDcHjdFeyKwGEjGlq9pmGVcvvbA",
-  token_expiry:"none"
-})
 const drive = google.drive({
   version:'v3',
-  auth: oauth2Client
+  auth
 })
-// Ensure upload directory exists.
-mkdirp.sync(UPLOAD_DIR)
 
 const storeUpload = async upload => {
   const { createReadStream, filename, mimetype } = await upload
     console.log(filename)
     const id = shortid.generate()
     let docId=""
-    let path = `https://docs.google.com/uc?id=${docId}&amp;export=download`
+    let path = `https://drive.google.com/uc?export=view&id=${docId}`
   
     // Store the file in google
     try{
     const res = await drive.files.create({
+      auth:auth,
       requestBody:{
         name: filename,
-        mimeType: mimetype
+        mimeType: mimetype,
+        parents:[folder]
       },
       media:{
         mimeType:mimetype,
@@ -40,7 +38,7 @@ const storeUpload = async upload => {
       }
     })
     docId= res.data.id
-    path = `https://docs.google.com/uc?id=${docId}&amp;export=download`
+    path = `https://drive.google.com/uc?export=view&id=${docId}`
     console.log("success uploading",id)
   }
   catch(e){
